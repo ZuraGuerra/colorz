f9a055f8fd4534bd11ad15a92c19e8534d5ac451
Zura Guerra <zuraweep@gmail.com>
2015-06-17 18:28:52 -0500

    Admin panel styles

--- api/controllers/AdminController.js
+++ api/controllers/AdminController.js
@@ -5,7 +5,7 @@ module.exports = {
 		var password = req.body.password;
 
 		console.log("Login params", req.body);
-		if (email === "zura@zura.zura" && password === "zura") {
+		if (email === "hashtag@allgo.mx" && password === "alggomx") {
 			console.log("Yaay");
 			res.send("true");
 		} else {
